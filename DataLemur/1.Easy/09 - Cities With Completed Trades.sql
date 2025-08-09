2f101d24603f4bd64799d95619a52fc805016180
george-ezat <e.georgeezat@gmail.com>
2025-08-09 07:08:00 +0300

    DataLemur Daily Challenge - Day 9

---
+++ DataLemur/1.Easy/09 - Cities With Completed Trades.sql
@@ -0,0 +1,14 @@
+SELECT
+    u.city,
+    COUNT(t.order_id) AS total_orders
+FROM
+    users AS u
+    JOIN trades AS t ON u.user_id = t.user_id
+WHERE
+    t.status = 'Completed'
+GROUP BY
+    u.city
+ORDER BY
+    total_orders DESC
+LIMIT
+    3;
